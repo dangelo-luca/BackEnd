47de7c2b936d100ce506a86c44c0e967ed31e9e2
Luca <dangelo.luca@iisgalvanimi.edu.it>
2024-05-03 18:22:30 +0200

    Fatto tutti i punti punti

--- Backend-4F/src/main/java/com/example/backend4f/ErroreServlet.java
+++ Backend-4F/src/main/java/com/example/backend4f/ErroreServlet.java
@@ -6,4 +6,15 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "Errore", value = "/Errore")
 public class ErroreServlet extends HttpServlet {
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String attributoSalvato = (String) session.getAttribute("Errore");
+        System.out.println(attributoSalvato)
+        response.setContentType("text/html");
+        PrintWriter oggetto = response.getWriter();
+        oggetto.println("<html><body>");
+        oggetto.println("<h1>" + attributoSalvato + "</h1>");
+        oggetto.println("</body></html>");
+    }
+
+
 }
